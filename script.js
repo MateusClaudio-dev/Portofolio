1b27a3d74d3a9ea02d86ae328fd1b0d817596201
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-05-13 21:44:49 -0300

    Refatorando o js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 let menu = document.querySelector('.cabecalho__mobile');
 let open = document.getElementById('open__menu');
 let exit = document.getElementById('exit__menu');
+let rodape = document.querySelector('.rodape')
 
 open.addEventListener('click', function() {
     menu.style.display = 'flex'
-    open.textHTML = 'ola'
+    rodape.style.display = 'none'
 })
 
 exit.addEventListener('click', function() {
     // Agora o estilo será aplicado corretamente
     menu.style.display = 'none';
+    rodape.style.display = 'block'
     
 });
 
